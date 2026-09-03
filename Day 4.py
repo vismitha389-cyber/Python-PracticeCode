c1e3631e31e4a9cb3cdf8b0e248cfe343173a274
Vismitha <vismitha389@gmail.com>
2026-09-03 11:36:04 +0530

    day 4

---
+++ Day 4.py
@@ -0,0 +1,27 @@
+#25.oprators
+a = 2
+b = 5
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+#26.even or odd
+num = int(input("enter the number:"))
+if num % 2 ==0:
+ print("even number")
+else:
+  print("odd number")
+
+#27.larger of two numbers
+a,b = 4,8
+if a > b:
+   print(a)
+else:
+    print(b)
